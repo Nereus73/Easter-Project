02d2f94a3ee152ca89c669cc069553b24f80cfdf
Balakosh <akos.balassa@live.com>
2025-03-08 15:47:59 +0100

    refactor: remove magic number for line feed

--- uart/uart.c
+++ uart/uart.c
@@ -5,6 +5,9 @@
 #include "ti/driverlib/dl_uart_extend.h"
 #include "ti_msp_dl_config.h"
 
+const uint8_t LINE_FEED = 0x0a;
+const uint8_t CARRIAGE_RETURN = 0x0d;
+
 void uart_transmit_blocking(const uint8_t* const data, const uint8_t length) {
     for (uint8_t i = 0; i < length; i++) {
         DL_UART_transmitDataBlocking(UART_0_INST, data[i]);
@@ -18,5 +21,5 @@ void uart_write_blocking(const char* const data) {
         DL_UART_transmitDataBlocking(UART_0_INST, data[i]);
     }
 
-    DL_UART_transmitDataBlocking(UART_0_INST, 0x0a);
+    DL_UART_transmitDataBlocking(UART_0_INST, LINE_FEED);
 }
