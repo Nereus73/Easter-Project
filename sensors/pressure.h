3d5619cebe1fb59aa2f50a8e3f2035e44f0b9a6e
Nereus73 <niklas_kueneke@gmx.de>
2025-03-16 08:54:13 +0100

    Added main Driver from Github with essential functions
    These include also simple Debug functions to test the function of the IC itself

--- sensors/pressure.h
+++ sensors/pressure.h
@@ -3,26 +3,6 @@
 #include <stdint.h>
 #include "ti/driverlib/dl_spi.h"
 
-// Register Map BMP390
-#define BMP390_CHIP_ID 0x00
-#define BMP390_REV_ID 0x01
-#define BMP390_ERR_REG 0x02
-#define BMP390_STATUS 0x03
-#define BMP390_DATA 0x04
-#define BMP390_SENSORTIME 0x0C
-#define BMP390_EVENT 0x10
-#define BMP390_INT_STATUS 0x11
-#define BMP390_FIFO_LENGTH 0x12
-#define BMP390_FIFO_DATA 0x14
-#define BMP390_FIFO_WTM 0x15
-#define BMP390_FIFO_CONFIG 0x17
-#define BMP390_INT_CTRL 0x19
-#define BMP390_IF_CONF 0x1A
-#define BMP390_PWR_CTRL 0x1B
-#define BMP390_OSR 0x1C
-#define BMP390_ODR 0x1D
-#define BMP390_CONFIG 0x1F
-#define BMP390_CMD 0x7E
 
 typedef struct  {
     uint8_t rxData[32];
@@ -35,6 +15,8 @@ typedef struct  {
 void initSensor(void);
 void BMP390_RegRead(uint8_t address, uint8_t count);
 void BMP390_RegWrite(uint8_t address, uint8_t data);
+uint8_t SPI_read(uint8_t reg_addr, uint8_t *read_data, uint32_t len, void *intf_ptr);
+uint8_t SPI_write(uint8_t reg_addr, uint8_t *reg_data, uint32_t len, void *intf_ptr);
 void RegisterWrite(SPIInterface* spiStruct);
 void RegisterRead(SPIInterface* spiStruct);
 void SPI_0_INST_IRQHandler(void);
